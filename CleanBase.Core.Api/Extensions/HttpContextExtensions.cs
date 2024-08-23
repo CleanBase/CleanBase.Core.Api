da43335709f672bedea5dffbb184abaf46b47426
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-23 14:14:43 +0700

    add extension

---
+++ CleanBase.Core.Api/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Api.Extensions
+{
+	public static class HttpContextExtensions
+	{
+		public static IPAddress GetRemoteIPAddress(this HttpContext context, bool allowForwarded = true)
+		{
+			IPAddress address;
+			return allowForwarded && IPAddress.TryParse(context.Request.Headers["CF-Connecting-IP"].FirstOrDefault<string>() ?? context.Request.Headers["X-Forwarded-For"].FirstOrDefault<string>(), out address) ? address : context.Connection.RemoteIpAddress;
+		}
+	}
+}
