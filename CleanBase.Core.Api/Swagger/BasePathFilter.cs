59a2ac55e0e1d05a695d45a37c2bb8f227c6b37a
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-23 14:15:42 +0700

    add pipline and license

---
+++ CleanBase.Core.Api/Swagger/BasePathFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Api.Swagger
+{
+	public class BasePathFilter : IDocumentFilter
+	{
+		private readonly string basePath;
+
+		public BasePathFilter(string basePath) => this.basePath = basePath;
+
+		public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+		{
+			swaggerDoc.Servers = (IList<OpenApiServer>)new List<OpenApiServer>()
+			   {
+				 new OpenApiServer() { Url = this.basePath }
+			   };
+		}
+	}
+}
